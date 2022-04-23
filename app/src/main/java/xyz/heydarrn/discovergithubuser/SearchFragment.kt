74c06ac03636ff7e759999d992be25ae6ab5d93d
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2022-04-24 00:00:00 +0700

    menambahkan layout untuk search user

---
+++ app/src/main/java/xyz/heydarrn/discovergithubuser/SearchFragment.kt
@@ -0,0 +1,31 @@
+package xyz.heydarrn.discovergithubuser
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import xyz.heydarrn.discovergithubuser.databinding.FragmentSearchBinding
+
+class SearchFragment : Fragment() {
+    private var _bindingSearchFragment:FragmentSearchBinding?=null
+    private val bindingSearch get() = _bindingSearchFragment
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _bindingSearchFragment= FragmentSearchBinding.inflate(inflater,container,false)
+        return bindingSearch?.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        bindingSearch?.inspectocatAtHome?.setImageResource(R.drawable.github_octocat_png_github_inspectocat_896)
+    }
+}
